2c036a8ea7f01d19997b89d7da7f43befee62e29
harriszk <zacharykeatonharris@gmail.com>
2025-09-18 19:08:55 -0400

    fix: changed content type from string to jsonb

--- backend/blog/src/main/java/com/zachary/blog/model/Post.java
+++ backend/blog/src/main/java/com/zachary/blog/model/Post.java
@@ -2,6 +2,14 @@ package com.zachary.blog.model;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import io.hypersistence.utils.hibernate.type.json.JsonBinaryType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,11 +32,18 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
-    private String content;
+
+    @Type(JsonBinaryType.class)
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private JsonNode content;
+    
     private String author;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
     // private Folder folder;
     // private List<Tag> tags;
 }
