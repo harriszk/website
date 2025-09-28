6c4854b1fc7d0ff60c95dcbb394ab56c31b2d2b2
harriszk <zacharykeatonharris@gmail.com>
2025-09-28 16:20:47 -0400

    fix: updated base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/website",
-    plugins: [react(), tailwindcss()]
+    base: "/",
+    plugins: [react(), tailwindcss()],
 });
