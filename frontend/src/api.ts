8c08ead9e6555f5caf06b021fb44855f2622edfa
harriszk <zacharykeatonharris@gmail.com>
2025-10-18 23:06:11 -0400

    feat: blogging frontend

---
+++ frontend/src/api.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import blogEndpoints from "./projects/blog/data/blogEndpoints.ts"
+
+const api = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_BASE_URL,
+    }),
+    endpoints: (build) => ({
+        ...blogEndpoints(build),
+    }),
+})
+
+export default api;
---
+++ frontend/src/projects/blog/data/blogEndpoints.ts
@@ -0,0 +1,27 @@
+import type { EndpointBuilder } from "@reduxjs/toolkit/query";
+
+const blogEndpoints = (build: EndpointBuilder<any, any, any>) => ({
+    getPosts: build.query<any, void>({
+        query: () => "/posts",
+    }),
+    getPostById: build.query({
+        query: (id: string) => `/posts/${id}`,
+        // keepUnusedDataFor: 0,
+    }),
+    createPost: build.mutation({
+        query: (newPost) => ({
+            url: "/posts",
+            method: "POST",
+            body: newPost,
+        }),
+    }),
+    deletePost: build.mutation({
+        query: (id: string) => ({
+            url: `/posts?id=${id}`,
+            method: "DELETE",
+        }),
+    }),
+});
+
+
+export default blogEndpoints;
